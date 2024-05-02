89f94a3de833412e5e1a17737875a8a8cd958bdd
ArtDinWin <45292877+ArtDinWin@users.noreply.github.com>
2024-05-02 22:14:54 +0300

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+const title = ref('123');
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped lang="scss">
+.read-the-docs {
+  color: #888;
+}
+</style>
