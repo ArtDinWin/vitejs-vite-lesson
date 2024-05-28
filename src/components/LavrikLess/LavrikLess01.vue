ec07421159fe438ebe1ae6991a7c4d545811bcf1
ArtDinWin <andsmirastrav@mail.ru>
2024-05-29 01:09:45 +0300

    Урок01 Директивы Синтаксис Options API, Composition API, script setup

---
+++ src/components/LavrikLess/LavrikLess01.vue
@@ -0,0 +1,52 @@
+<script>
+// У script setup будет отсутствовать components:
+import SampleLess from './less1/01SampleLess.vue'
+import CompositionApi from './less1/02CompositionApi.vue'
+import ScriptSetup from './less1/02ScriptSetup.vue'
+import MethodsLess from './less1/03Methods.vue'
+
+export default {
+  data() {
+    return {
+      title: "Начало работа с Vue"
+    }
+  },
+  components: {
+    SampleLess,
+    CompositionApi, 
+    ScriptSetup,
+    MethodsLess
+    
+  }
+}
+</script>
+
+<template>
+
+<h3>{{ title }}</h3>
+<ul class="description-list">
+<li class="description-item">01-simple</li>
+<li class="description-item">02-directive</li>
+<li class="description-item">03-methods</li>
+<li class="description-item">04-methods-so-so</li>
+<li class="description-item">05-computed</li>
+<li class="description-item">06-computed-sale</li>
+<li class="description-item">07-computed-async</li>
+<li class="description-item">08-watch</li>
+<li class="description-item">09-for</li>
+<li class="description-item">10-home-work</li>
+<li class="description-item">11-cake</li>
+</ul>
+<hr>
+<SampleLess></SampleLess>
+<CompositionApi></CompositionApi>
+<ScriptSetup></ScriptSetup>
+<MethodsLess></MethodsLess>
+
+</template>
+
+<style scoped lang="scss">
+.description-list {
+    text-align: left;
+}
+</style>
---
+++ src/components/LavrikLess/less1/02ScriptSetup.vue
@@ -0,0 +1,49 @@
+<script setup>
+// нет export default
+// нет data() вместо этого ref и reactive
+// отсутствует setup() 
+// нет methods это обычные функции
+// computed через import
+// хуки через import onMounted
+
+import { ref, computed, onMounted  } from 'vue'
+
+const title = ref("02 Script setup");
+const name = ref("");
+const count = ref(0);
+
+function increment() {
+  count.value++
+    }
+
+const doubledCount = computed(() => count.value * 2);
+
+ onMounted (() => {
+    console.log(`компонент 02ScriptSetup теперь примонтирован.`)
+  }   ) 
+</script>
+
+<template>
+<div class="section">
+<h3>{{ title }}</h3>
+<ul class="description-list">
+  <li class="description-item">Composition API</li>
+</ul>
+<hr>
+<div class="wrapper">
+		<div class="sample">
+			<!-- <input type="text" v-model="name"> -->
+      <h5>Hello, {{ name }}</h5>
+      <input type="text" :value="name" @input="name = $event.target.value">				
+		</div>
+	</div>
+  <hr>
+</div>
+</template>
+
+<style scoped lang="scss">
+.description-list {
+    text-align: left;
+}
+
+</style>
