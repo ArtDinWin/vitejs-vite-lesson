c78c7590fe283e0164be4804410cdf5577657c8f
ArtDinWin <andsmirastrav@mail.ru>
2024-05-29 17:53:13 +0300

    Урок01 computed / async, whatch

---
+++ src/components/LavrikLess/less1/04-methods-so-so.vue
@@ -0,0 +1,80 @@
+<script>
+export default {
+  data(){
+				return {
+					showAlert: false,
+          title: "04-methods-so-so",
+					firstName: '',
+					lastName: '',
+          name: ""
+				}
+			},
+      methods: {
+				fullNameMethods(){
+					console.log('here');
+					return (this.firstName + ' ' + this.lastName).trim();
+				},
+        // some bad, на грани или за гранью философии vue...
+				toUpper(str){
+					return str.toUpperCase().split('').reverse().join('');
+				}
+			},
+      computed: {
+				fullNameComputed(){
+					console.log('here2');
+					return (this.firstName + ' ' + this.lastName).trim();
+				}
+			}
+      ,
+  mounted() {
+
+  }
+}
+
+</script>
+
+<template>
+<div class="section">
+<h3>{{ title }}</h3>
+<ul class="description-list">
+  <li class="description-item">Не удачное использование 
+    (Проблема - вызывается при любом изменении
+  если мы вызываем через methods, оно пересчитывается всегда даже при изменении 
+  name которое не учавствует в  fullName. Решение это computed - вычисляемое свойство.
+  Вызов через Methods  fullNameMethods() круглые скобки, 
+  Вызов через Computed   fullNameComputed без круглых скобок - нельзя передать аргумент (создается новый Computed)
+  </li>
+</ul>
+<hr>
+<div class="wrapper">
+		<div class="sample">
+			FN: <input type="text" v-model.trim="firstName"><br>
+			LN: <input type="text" v-model.trim="lastName"><br>
+			<hr>
+			<h5>Methods: Hello, {{ fullNameMethods() }}</h5>
+      <h5>Computed: Hello, {{ fullNameComputed }}</h5>
+			<hr>
+			<button @click="showAlert = !showAlert" class="btn btn-success" type="button">Toggle alert</button>
+			<div class="alert alert-danger mt-3" v-show="showAlert">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt porro iste incidunt optio maxime eos vero cupiditate delectus in velit quae neque aliquid facilis nobis repellendus, quidem eveniet! Nihil, cum.
+			</div>
+		</div>
+	</div>
+  <div class="wrapper">
+		<div class="sample">
+			<!-- <input type="text" v-model="name"> -->
+      <h5>Hello, {{ name }}</h5>
+      <input type="text" v-model="name">				
+		</div>
+	</div>
+  {{ toUpper(firstName) }}
+			{{ toUpper(lastName) }}
+  <hr>
+</div>
+</template>
+
+<style scoped lang="scss">
+.description-list {
+    text-align: left;
+}
+</style>
---
+++ src/components/LavrikLess/less1/07-computed-async.vue
@@ -0,0 +1,104 @@
+<script>
+export default {
+	data(){
+				return {
+					promo: '',
+					price: 1000,
+					sale: 0
+				}
+			},
+			computed: {
+				hasSale(){
+					return this.sale > 0;
+				},
+				total(){
+					return this.price * (1 - this.sale / 100);
+				}
+			},
+			methods: {
+				checkSale(){
+					this.getSale(this.promo, sale => {
+						this.sale = sale;
+						// callback setTimeout его контекст объект windows
+						// поэтому без стрелочной функции this.sale будет ссылаться на неправильный объект
+						// стрелочная функция решает проблему фиксирует контекст где вызвана
+					});
+				},
+				getSale(promo, fn){
+					setTimeout(function(){
+						let codes = {
+							some: 10,
+							other: 20
+						};
+
+						let sale = codes.hasOwnProperty(promo) ? codes[promo] : 0;
+						fn(sale);
+					}, 500);
+				}
+			} ,
+  mounted() {
+
+  }
+}
+		/*
+		function getSale(promo, fn){
+			setTimeout(() => {
+				let codes = {
+					some: 10,
+					other: 20
+				};
+
+				let sale = codes.hasOwnProperty(promo) ? codes[promo] : 0;
+				fn(sale);
+			}, 500);
+		}
+
+		getSale('promo', function(sale){
+			console.log(sale);
+		})
+
+		*/
+</script>
+
+<template>
+<div class="section">
+<h3>{{ title }}</h3>
+<ul class="description-list">
+<li class="description-item">
+	Скидка расчитываемая от промокода - функция getSale(promo) methods
+	<br>
+	Корректный промокод 'some' / 'other' + Enter (убрать фокус) из-за v-model.lazy
+	<br>{{ '<input type="text" v-model.trim.lazy="promo">' }}
+</li>
+<li>
+	computed Бесполезны когда функция обращается к серверу за данными Ассинхронные  computed
+<br>
+Два способа получить ответ 1) Promise (asyn await синтаксический сахар используемый Promice) 2) callback функции
+</li>
+<li>введем эмуляцию задержки от сервера с помощью callback (метод @change="checkSale" ближе к JavaScript)</li>
+<li>
+	<b>Итог:</b><br> - разбиваем computed на мелкие, чтобы переиспользовать в шаблоне (пример со скидкой по промокоду) <br>- computed могут зависить от data() могут от других computed каскадно
+  </li>
+</ul>
+<hr>
+	<div class="wrapper">
+		<div class="sample">
+			<input type="text" v-model.trim.lazy="promo" @change="checkSale"><br>
+			<hr>
+			{{ price }}
+			<div v-if="hasSale">
+				<div class="alert alert-danger">- {{ sale }}%</div>
+				{{ total }}
+			</div>			
+		</div> 
+	</div>
+
+  <hr>
+</div>
+</template>
+
+<style scoped lang="scss">
+.description-list {
+    text-align: left;
+}
+</style>
